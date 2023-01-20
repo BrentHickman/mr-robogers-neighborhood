c0907c546bca8b2a29a489c471da8c551711cf00
BrentHickman <brenthickman@ymail.com>
2023-01-20 15:19:27 -0800

    Re-do test 2 after realizing for loop was not working as expected. Changed to forEach

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,27 @@
+//Business Logic
 
 function beepBoop(number) {
   let beepBoopArray = [];
   for (let i = 0; i <= number; i++) {
-    beepBoopArray.push(i);
+      beepBoopArray.push(i);
+    }
+
+  return boopBeep(beepBoopArray);
+
   }
-  return beepBoopArray;
+
+function boopBeep(array) {
+  outputArray = [];
+  array.forEach(element => {
+    if(element.toString().includes("1")) {
+      outputArray.push("Beep!");
+    } else {
+      outputArray.push(element);
+    }
+    
+  });
+console.log(outputArray);
 }
+
+//User Interface Logic
+
